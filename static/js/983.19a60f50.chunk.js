258dabce9ea7696b7ccf0f70770d59bd59c39258
MaksymDpUa <117561870+MaksymDpUa@users.noreply.github.com>
2023-08-05 16:48:40 +0000

    Deploying to gh-pages from @ MaksymDpUa/goit-react-hw-08-phonebook@2735ba0cdad268e948e8cf5296903ed8ab375d70 🚀

---
+++ static/js/983.19a60f50.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)("h1",{children:"Welcome, to my App"})}}}]);
+//# sourceMappingURL=983.19a60f50.chunk.js.map
